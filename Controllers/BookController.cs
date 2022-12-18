2bc8caf9cb9866b81338fada28fd2e635c77f6fa
theLahcim <michal.mazur32@gmail.com>
2022-12-18 21:55:14 +0100

    feat(BookController): view Book list

---
+++ Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using ebooklist.Data;
+using ebooklist.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ebooklist.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BookController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var books = _context.Books.ToList();
+            List<BookViewModel> bookList = new List<BookViewModel>();
+
+            if (books != null)
+            {
+                foreach(var book in books)
+                {
+                    var BookViewModel = new BookViewModel()
+                    {
+                        Author = book.Author,
+                        Title = book.Title,
+                        Cover = book.Cover,
+                        Pages = book.Pages,
+                        Price = book.Price,
+                        Series = book.Series,
+                        Publisher = book.Publisher,
+                        Year = book.Year,
+                        Shop_url = book.Shop_url,
+                        Img_url = book.Img_url
+                    };
+                    bookList.Add(BookViewModel);
+                }
+            }
+            return View(bookList);
+        }
+    }
+}
---
+++ Models/BookViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ebooklist.Models
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public float Price { get; set; }
+        public string Publisher { get; set; }
+        public string Series { get; set; }
+        public int Year { get; set; }
+        public int Pages { get; set; }
+        public string Cover { get; set; }
+        public string Shop_url { get; set; }
+        public string Img_url { get; set; }
+    }
+}
