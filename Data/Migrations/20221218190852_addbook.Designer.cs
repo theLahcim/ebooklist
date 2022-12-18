c54fd81948ad53a16a7996bea2dc08889198433c
theLahcim <michal.mazur32@gmail.com>
2022-12-18 20:52:42 +0100

    feat(Book): add class Book

--- Data/Migrations/20221218190852_addbook.Designer.cs
+++ Data/Migrations/20221218190852_addbook.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ebooklist.Data;
 
 namespace ebooklist.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221218190852_addbook")]
+    partial class addbook
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
