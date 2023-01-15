4fddddc6134c833cd7690fd4cf7fa7ffb8906129
theLahcim <michal.mazur32@gmail.com>
2023-01-15 22:25:26 +0100

    feat(Pagination): add Pagination

--- Entieties/Book.cs
+++ Entieties/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,13 +10,13 @@ namespace ebooklist.Entieties
     public class Book
     {
         [Key]
-        public int ISBN13 { get; set; }
-        public int ISBN10 { get; set; }
+        public string ISBN13 { get; set; }
+        public string ISBN10 { get; set; }
         public string Title { get; set; }
         public string Subtitle { get; set; }
-        public string Author { get; set; }
+        public string Authors { get; set; }
         public string Categories { get; set; }
-        public string Tumbnail { get; set; }
+        public string Thumbnail { get; set; }
         public string Description { get; set; }
         public int Published_year { get; set; }
         public float Average_rating { get; set; }
